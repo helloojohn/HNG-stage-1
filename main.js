97b0e0e5647fd748bb5a51c7fa4c12b144f598d3
John Okorie <ameachijohn001@gmail.com>
2023-09-07 16:08:22 +0100

    Stage one initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+	// Get the elements
+	const dayElement = document.querySelector(
+		'[data-testid="currentDayOfTheWeek"]'
+	);
+	const timeElement = document.querySelector('[data-testid="currentUTCTime"]');
+
+	// Get current day of the week
+	const daysOfWeek = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	const today = new Date();
+	const dayOfWeek = daysOfWeek[today.getUTCDay()];
+
+	// Get current UTC time
+	const hours = today.getUTCHours().toString().padStart(2, "0");
+	const minutes = today.getUTCMinutes().toString().padStart(2, "0");
+	const seconds = today.getUTCSeconds().toString().padStart(2, "0");
+	const currentTime = `${hours}:${minutes}:${seconds}`;
+
+	// Update the elements
+	dayElement.textContent = `Current Day: ${dayOfWeek}`;
+	timeElement.textContent = `Current UTC Time: ${currentTime}`;
+});
